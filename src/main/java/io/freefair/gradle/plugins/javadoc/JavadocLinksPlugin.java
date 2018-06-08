e902e01e60268f4ed895bf35e83914edf33806ff
Lars Grefer <github@larsgrefer.de>
2018-06-08 17:00:36 +0200

    improve javadoc links

--- src/main/java/io/freefair/gradle/plugins/javadoc/JavadocLinksPlugin.java
+++ src/main/java/io/freefair/gradle/plugins/javadoc/JavadocLinksPlugin.java
@@ -5,8 +5,6 @@ import org.gradle.api.JavaVersion;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.ResolvedArtifact;
-import org.gradle.api.artifacts.component.ComponentIdentifier;
 import org.gradle.api.artifacts.component.ModuleComponentIdentifier;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.internal.file.UnionFileCollection;
@@ -15,7 +13,9 @@ import org.gradle.api.tasks.javadoc.Javadoc;
 import org.gradle.external.javadoc.MinimalJavadocOptions;
 import org.gradle.external.javadoc.StandardJavadocDocletOptions;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Getter
 public class JavadocLinksPlugin implements Plugin<Project> {
@@ -26,41 +26,40 @@ public class JavadocLinksPlugin implements Plugin<Project> {
     public void apply(Project project) {
         this.project = project;
 
-        project.getTasks().withType(Javadoc.class, javadoc -> javadoc.doFirst(task -> {
+        project.getTasks().withType(Javadoc.class, javadoc -> {
 
             addLink(javadoc, getJavaSeLink(JavaVersion.current()));
 
-            Configuration configuration = findConfiguraion(javadoc.getClasspath());
+            javadoc.doFirst(task -> {
+                findConfigurations(javadoc.getClasspath())
+                        .flatMap(files -> files.getResolvedConfiguration().getResolvedArtifacts().stream())
+                        .map(resolvedArtifact -> resolvedArtifact.getId().getComponentIdentifier())
+                        .filter(ModuleComponentIdentifier.class::isInstance)
+                        .map(ModuleComponentIdentifier.class::cast)
+                        .map(moduleComponentIdentifier -> {
+                            String group = moduleComponentIdentifier.getGroup();
+                            String artifact = moduleComponentIdentifier.getModule();
+                            String version = moduleComponentIdentifier.getVersion();
 
-            boolean usedJavadocIo = false;
+                            String wellKnownLink = findWellKnownLink(group, artifact, version);
+                            if (wellKnownLink != null) {
+                                javadoc.getLogger().info("Using well known link '{}' for '{}:{}:{}'", wellKnownLink, group, artifact, version);
+                                return wellKnownLink;
+                            } else {
+                                javadoc.getLogger().info("Using javadoc.io link for '{}:{}:{}'", group, artifact, version);
+                                return String.format("https://static.javadoc.io/%s/%s/%s/", group, artifact, version);
+                            }
+                        })
+                        .forEach(link -> addLink(javadoc, link));
 
-            for (ResolvedArtifact resolvedArtifact : configuration.getResolvedConfiguration().getResolvedArtifacts()) {
-                ComponentIdentifier componentIdentifier = resolvedArtifact.getId().getComponentIdentifier();
+                boolean usedJavadocIo = getLinks(javadoc).stream()
+                        .anyMatch(link -> link.startsWith("https://static.javadoc.io"));
 
-                if (componentIdentifier instanceof ModuleComponentIdentifier) {
-                    ModuleComponentIdentifier moduleComponentIdentifier = (ModuleComponentIdentifier) componentIdentifier;
-                    String group = moduleComponentIdentifier.getGroup();
-                    String artifact = moduleComponentIdentifier.getModule();
-                    String version = moduleComponentIdentifier.getVersion();
-
-                    String wellKnownLink = findWellKnownLink(group, artifact, version);
-                    if (wellKnownLink != null) {
-                        javadoc.getLogger().info("Using well known link '{}' for '{}:{}:{}'", wellKnownLink, group, artifact, version);
-                        addLink(javadoc, wellKnownLink);
-                        break;
-                    } else {
-                        javadoc.getLogger().info("Using javadoc.io link for '{}:{}:{}'", group, artifact, version);
-                        String javadocIoLink = String.format("https://static.javadoc.io/%s/%s/%s/", group, artifact, version);
-                        addLink(javadoc, javadocIoLink);
-                        usedJavadocIo = true;
-                    }
+                if (usedJavadocIo && javadoc.getOptions().getJFlags().stream().noneMatch(flag -> flag.contains("http.agent"))) {
+                    javadoc.getOptions().jFlags("-Dhttp.agent=" + System.currentTimeMillis());
                 }
-            }
-
-            if (usedJavadocIo && javadoc.getOptions().getJFlags().stream().noneMatch(flag -> flag.contains("http.agent"))) {
-                javadoc.getOptions().jFlags("-Dhttp.agent=" + System.currentTimeMillis());
-            }
-        }));
+            });
+        });
     }
 
     private void addLink(Javadoc javadoc, String link) {
@@ -79,45 +78,88 @@ public class JavadocLinksPlugin implements Plugin<Project> {
         }
     }
 
+    private List<String> getLinks(Javadoc javadoc) {
+        MinimalJavadocOptions options = javadoc.getOptions();
+        if (options instanceof StandardJavadocDocletOptions) {
+            return ((StandardJavadocDocletOptions) options).getLinks();
+        }
+        return Collections.emptyList();
+    }
+
     private String findWellKnownLink(String group, String artifact, String version) {
+
+        if (group.equals("javax") && artifact.equals("javaee-api") && version.matches("[567]\\..*")) {
+            return "https://docs.oracle.com/javaee/" + version.substring(0, 1) + "/api/";
+        }
+
+        if (group.equals("javax") && artifact.equals("javaee-api") && version.startsWith("8")) {
+            return "https://javaee.github.io/javaee-spec/javadocs/";
+        }
+
         if (group.equals("org.springframework") && artifact.startsWith("spring-")) {
             return "https://docs.spring.io/spring/docs/" + version + "/javadoc-api/";
-        } else if (group.equals("org.springframework.boot")) {
+        }
+
+        if (group.equals("org.springframework.boot") && artifact.startsWith("spring-boot")) {
             return "https://docs.spring.io/spring-boot/docs/" + version + "/api/";
         }
+
+        if (group.equals("org.springframework.security") && artifact.startsWith("spring-security")) {
+            return "https://docs.spring.io/spring-security/site/docs/" + version + "/api/";
+        }
+
+        if (group.equals("org.springframework.data") && artifact.equals("spring-data-jpa")) {
+            return "https://docs.spring.io/spring-data/jpa/docs/" + version + "/api/";
+        }
+
+        if (group.equals("org.springframework.webflow") && artifact.equals("spring-webflow")) {
+            return "https://docs.spring.io/spring-webflow/docs/" + version + "/api/";
+        }
+
+        if (group.equals("com.squareup.okio") && version.startsWith("1.")) {
+            return "https://square.github.io/okio/1.x/" + artifact + "/";
+        }
+
+        if (group.equals("com.squareup.okhttp3")) {
+            return "https://square.github.io/okhttp/3.x/" + artifact + "/";
+        }
+
+        if (group.equals("com.squareup.retrofit")) {
+            return "https://square.github.io/retrofit/1.x/retrofit/";
+        }
+
+        if (group.equals("com.squareup.retrofit2")) {
+            return "https://square.github.io/retrofit/2.x/" + artifact + "/";
+        }
+
+        if (group.equals("org.hibernate") && artifact.equals("hibernate-core")) {
+            return "https://docs.jboss.org/hibernate/orm/" + version.substring(0, 3) + "/javadocs/";
+        }
+
+        if ((group.equals("org.hibernate") || group.equals("org.hibernate.validator")) && artifact.equals("hibernate-validator")) {
+            return "https://docs.jboss.org/hibernate/validator/" + version.substring(0, 3) + "/api/";
+        }
+
         return null;
     }
 
-    private Configuration findConfiguraion(FileCollection classpath) {
+    private Stream<Configuration> findConfigurations(FileCollection classpath) {
         if (classpath instanceof Configuration) {
-            return (Configuration) classpath;
+            return Stream.of((Configuration) classpath);
         } else if (classpath instanceof UnionFileCollection) {
-            for (FileCollection files : ((UnionFileCollection) classpath).getSources()) {
-                Configuration configuraion = findConfiguraion(files);
-                if (configuraion != null) return configuraion;
-            }
+            return ((UnionFileCollection) classpath).getSources()
+                    .stream()
+                    .flatMap(this::findConfigurations);
         }
-
-        return null;
+        return Stream.empty();
     }
 
     private String getJavaSeLink(JavaVersion javaVersion) {
         switch (javaVersion) {
-            case VERSION_1_5:
-                return "https://docs.oracle.com/javase/5/docs/api/";
-            case VERSION_1_6:
-                return "https://docs.oracle.com/javase/6/docs/api/";
-            case VERSION_1_7:
-                return "https://docs.oracle.com/javase/7/docs/api/";
-            case VERSION_1_8:
-                return "https://docs.oracle.com/javase/8/docs/api/";
-            case VERSION_1_9:
-                return "https://docs.oracle.com/javase/9/docs/api/";
-            case VERSION_1_10:
-                return "https://docs.oracle.com/javase/10/docs/api/";
+            case VERSION_11:
+                return "https://download.java.net/java/early_access/jdk11/docs/api/";
             default:
-                project.getLogger().warn("Unknown java version {}", javaVersion);
-                return null;
+                return "https://docs.oracle.com/javase/" + javaVersion.getMajorVersion() + "/docs/api/";
         }
     }
 }
