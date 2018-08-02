cea58b2414eea5e486493e77a80448ee1c11948e
Lars Grefer <github@larsgrefer.de>
2018-08-02 23:13:07 +0200

    Update to Gradle 4.9 and Lombok 1.18.2

--- lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/LombokExtension.java
+++ lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/LombokExtension.java
@@ -8,7 +8,7 @@ import java.util.TreeMap;
 @Data
 public class LombokExtension {
 
-    private String version = "1.18.0";
+    private String version = "1.18.2";
 
     private Map<String, String> config = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 
