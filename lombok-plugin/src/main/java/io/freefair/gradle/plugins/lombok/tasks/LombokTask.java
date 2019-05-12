bd8bef63d798b6dbe64ed34cbed079ba08ceace6
Lars Grefer <github@larsgrefer.de>
2019-05-12 22:34:18 +0200

    Annotate all Task properties

--- lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/tasks/LombokTask.java
+++ lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/tasks/LombokTask.java
@@ -2,8 +2,10 @@ package io.freefair.gradle.plugins.lombok.tasks;
 
 import org.gradle.api.Task;
 import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.tasks.Classpath;
 
 public interface LombokTask extends Task {
 
+    @Classpath
     ConfigurableFileCollection getLombokClasspath();
 }
