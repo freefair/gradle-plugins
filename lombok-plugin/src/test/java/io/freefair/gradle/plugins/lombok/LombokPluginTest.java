ccfc8923df5316d9b10c2eed8a6020d384ad7cbd
Lars Grefer <github@larsgrefer.de>
2020-09-30 01:11:23 +0200

    Add unit test for sonarqube integration

--- lombok-plugin/src/test/java/io/freefair/gradle/plugins/lombok/LombokPluginTest.java
+++ lombok-plugin/src/test/java/io/freefair/gradle/plugins/lombok/LombokPluginTest.java
@@ -7,6 +7,8 @@ import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LombokPluginTest {
 
     private Project project;
@@ -31,5 +33,19 @@ public class LombokPluginTest {
     public void apply_before_java() {
         project.getPlugins().apply(LombokPlugin.class);
         project.getPlugins().apply(JavaPlugin.class);
+    }
+
+    @Test
+    public void with_sonar() {
+        project.getPlugins().apply("java");
+        project.getPlugins().apply(LombokPlugin.class);
+
+        LombokExtension lombokExtension = project.getExtensions().getByType(LombokExtension.class);
+
+        assertThat(lombokExtension).isNotNull();
+        assertThat(lombokExtension.getConfig().get()).doesNotContainKey("lombok.extern.findbugs.addSuppressFBWarnings");
+
+        project.getPlugins().apply("org.sonarqube");
+        assertThat(lombokExtension.getConfig().get()).containsEntry("lombok.extern.findbugs.addSuppressFBWarnings", "true");
     }
 }
