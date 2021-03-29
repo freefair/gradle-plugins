a3bc8b69242f2bd7f1d06ef62083f07ca1443040
Lars Grefer <github@larsgrefer.de>
2021-03-29 05:46:49 +0200

    Mark config-related members as deprecated

--- lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/LombokExtension.java
+++ lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/LombokExtension.java
@@ -2,6 +2,7 @@ package io.freefair.gradle.plugins.lombok;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
@@ -12,6 +13,7 @@ import javax.inject.Inject;
  * @author Lars Grefer
  * @see LombokPlugin
  */
+@Slf4j
 @Getter
 @Setter
 public class LombokExtension {
@@ -24,11 +26,18 @@ public class LombokExtension {
     /**
      * Additional Entries for the lombok.config file.
      */
+    @Deprecated
     private final MapProperty<String, String> config;
 
     @Inject
     public LombokExtension(ObjectFactory objectFactory) {
         version = objectFactory.property(String.class).convention("1.18.18");
         config = objectFactory.mapProperty(String.class, String.class);
+    }
+
+    @Deprecated
+    public MapProperty<String, String> getConfig() {
+        log.warn("'lombok.config' is deprecated", new Exception("stacktrace"));
+        return config;
     }
 }
