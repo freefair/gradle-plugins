72d3ae00c2f1266f6532f54f5782d70f8b03dc10
Lars Grefer <github@larsgrefer.de>
2021-08-26 02:34:43 +0200

    Use extension instead of convention in compress-plugin

--- compress-plugin/src/main/java/io/freefair/gradle/plugins/compress/CompressTreePlugin.java
+++ compress-plugin/src/main/java/io/freefair/gradle/plugins/compress/CompressTreePlugin.java
@@ -10,6 +10,9 @@ import org.gradle.api.Project;
 public class CompressTreePlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
+        //noinspection deprecation
         project.getConvention().create(CompressFileOperations.class, "compressTree", CompressFileOperationsImpl.class, project);
+
+        project.getExtensions().create(CompressFileOperations.class, "commonsCompress", CompressFileOperationsImpl.class, project);
     }
 }
