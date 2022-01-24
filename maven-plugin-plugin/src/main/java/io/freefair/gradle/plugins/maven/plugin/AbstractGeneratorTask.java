14a6ac3a997f19bedc08c6a6ca6887dd5b284fb5
Lars Grefer <github@larsgrefer.de>
2022-01-24 01:08:53 +0100

    Update to Maven 3.6.2

--- maven-plugin-plugin/src/main/java/io/freefair/gradle/plugins/maven/plugin/AbstractGeneratorTask.java
+++ maven-plugin-plugin/src/main/java/io/freefair/gradle/plugins/maven/plugin/AbstractGeneratorTask.java
@@ -5,9 +5,6 @@ import io.freefair.gradle.plugins.maven.plugin.wrappers.MojoAnnotationScannerWra
 import io.freefair.gradle.plugins.maven.plugin.wrappers.PlexusLoggerWrapper;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.maven.artifact.factory.DefaultArtifactFactory;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
-import org.apache.maven.artifact.resolver.DefaultArtifactResolver;
 import org.apache.maven.plugin.descriptor.InvalidPluginDescriptorException;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.plugin.plugin.AbstractGeneratorMojo;
@@ -46,6 +43,7 @@ import java.util.stream.Collectors;
  * @author Lars Grefer
  * @see AbstractGeneratorMojo
  */
+@SuppressWarnings("JavadocReference")
 @Getter
 @Setter
 public abstract class AbstractGeneratorTask extends DefaultTask {
@@ -145,24 +143,36 @@ public abstract class AbstractGeneratorTask extends DefaultTask {
     @Internal
     protected abstract File getBaseDir();
 
+    /**
+     * @see AbstractGeneratorMojo#mojoScanner
+     */
     private MojoScanner getMojoScanner() {
         Map<String, MojoDescriptorExtractor> extractors = new TreeMap<>();
+
+        extractors.put("java-annotations", getMojoDescriptorExtractor());
+
+        DefaultMojoScanner defaultMojoScanner = new DefaultMojoScanner(extractors);
+        defaultMojoScanner.enableLogging(new PlexusLoggerWrapper(getLogger()));
+        return defaultMojoScanner;
+    }
+
+    private JavaAnnotationsMojoDescriptorExtractor getMojoDescriptorExtractor() {
+        PlexusLoggerWrapper plexusLoggerWrapper = new PlexusLoggerWrapper(getLogger());
+
         JavaAnnotationsMojoDescriptorExtractor mojoDescriptorExtractor = new JavaAnnotationsMojoDescriptorExtractor();
+        mojoDescriptorExtractor.enableLogging(plexusLoggerWrapper);
 
         DefaultMojoAnnotationsScanner delegate = new DefaultMojoAnnotationsScanner();
-        delegate.enableLogging(new PlexusLoggerWrapper(getLogger()));
+        delegate.enableLogging(plexusLoggerWrapper);
+
         MojoAnnotationScannerWrapper mojoAnnotationsScanner = new MojoAnnotationScannerWrapper(delegate);
         mojoAnnotationsScanner.setSourceDirectories(sourceDirectories);
         mojoAnnotationsScanner.setClassesDirectories(classesDirectories);
 
-        ArtifactResolver artifactResolver = new DefaultArtifactResolver();
-        DefaultArtifactFactory artifactFactory = new DefaultArtifactFactory();
         ArchiverManager archiverManager = new DefaultArchiverManager();
 
         Map<String, Object> values = new HashMap<>();
         values.put("mojoAnnotationsScanner", mojoAnnotationsScanner);
-        values.put("artifactResolver", artifactResolver);
-        values.put("artifactFactory", artifactFactory);
         values.put("archiverManager", archiverManager);
 
         try {
@@ -176,9 +186,6 @@ public abstract class AbstractGeneratorTask extends DefaultTask {
             throw new RuntimeException(e);
         }
 
-        extractors.put("java-annotations", mojoDescriptorExtractor);
-        DefaultMojoScanner defaultMojoScanner = new DefaultMojoScanner(extractors);
-        defaultMojoScanner.enableLogging(new PlexusLoggerWrapper(getLogger()));
-        return defaultMojoScanner;
+        return mojoDescriptorExtractor;
     }
 }
