3b0244d6fccae27536906fa79c601ea38199d1df
Lars Grefer <github@larsgrefer.de>
2022-05-13 01:44:43 +0200

    Remove deprecated lombok.config field

--- lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/LombokExtension.java
+++ lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/LombokExtension.java
@@ -3,9 +3,7 @@ package io.freefair.gradle.plugins.lombok;
 import lombok.Getter;
 import lombok.Setter;
 import org.gradle.api.model.ObjectFactory;
-import org.gradle.api.provider.MapProperty;
 import org.gradle.api.provider.Property;
-import org.gradle.internal.deprecation.DeprecationLogger;
 
 import javax.inject.Inject;
 
@@ -24,29 +22,13 @@ public class LombokExtension {
      */
     private final Property<String> version;
 
-    /**
-     * Additional Entries for the lombok.config file.
-     */
-    @Deprecated
-    private final MapProperty<String, String> config;
-
     private final Property<Boolean> disableConfig;
 
     @Inject
     public LombokExtension(ObjectFactory objectFactory) {
         version = objectFactory.property(String.class).convention(LOMBOK_VERSION);
-        config = objectFactory.mapProperty(String.class, String.class);
         disableConfig = objectFactory.property(Boolean.class);
 
         disableConfig.convention(System.getProperty("lombok.disableConfig") != null);
-    }
-
-    @Deprecated
-    public MapProperty<String, String> getConfig() {
-        DeprecationLogger.deprecateProperty(LombokExtension.class, "config")
-                .willBeRemovedInGradle8()
-                .undocumented()
-                .nagUser();
-        return config;
     }
 }
