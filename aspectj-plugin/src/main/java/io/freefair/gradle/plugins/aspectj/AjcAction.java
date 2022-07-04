0a9e5ebccfb966044fefe1e3d94469f179c508d6
Lars Grefer <github@larsgrefer.de>
2022-07-05 01:36:27 +0200

    Skip AJC action for special conditions

--- aspectj-plugin/src/main/java/io/freefair/gradle/plugins/aspectj/AjcAction.java
+++ aspectj-plugin/src/main/java/io/freefair/gradle/plugins/aspectj/AjcAction.java
@@ -4,10 +4,12 @@ import io.freefair.gradle.plugins.aspectj.internal.AspectJCompileSpec;
 import io.freefair.gradle.plugins.aspectj.internal.AspectJCompiler;
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.SneakyThrows;
 import org.gradle.api.Action;
 import org.gradle.api.NonNullApi;
 import org.gradle.api.Task;
 import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.file.ProjectLayout;
 import org.gradle.api.model.ObjectFactory;
 import org.gradle.api.provider.Property;
@@ -21,8 +23,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile;
 
 import javax.inject.Inject;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static io.freefair.gradle.util.TaskUtils.*;
 
@@ -91,7 +99,7 @@ public class AjcAction implements Action<Task> {
 
     @Override
     public void execute(Task task) {
-        if (!enabled.getOrElse(true)) {
+        if (shouldSkipAjc(task)) {
             return;
         }
 
@@ -100,23 +108,60 @@ public class AjcAction implements Action<Task> {
         new AspectJCompiler(javaExecHandleFactory).execute(spec);
     }
 
+    private boolean shouldSkipAjc(Task task) {
+        if (!enabled.getOrElse(true)) {
+            task.getLogger().lifecycle("Skipping AJC (disabled)");
+            return true;
+        }
+
+        if (!getOptions().getAspectpath().isEmpty()) {
+            task.getLogger().info("Not skipping AJC (aspectpath is set)");
+            return false;
+        }
+
+        if (!getOptions().getInpath().isEmpty()) {
+            task.getLogger().info("Not skipping AJC (inpath is set)");
+            return false;
+        }
+
+        if (!getOptions().getSourceroots().isEmpty()) {
+            task.getLogger().info("Not skipping AJC (sourceroots is set)");
+            return false;
+        }
+
+        URL[] cp = getCompileClasspath(task)
+                .map(AjcAction::toURL)
+                .toArray(URL[]::new);
+
+        try (URLClassLoader urlClassLoader = new URLClassLoader(cp)) {
+            Class<?> clazz = urlClassLoader.loadClass("org.aspectj.lang.JoinPoint");
+            task.getLogger().info("Not skipping AJC (found {} in {})", clazz, clazz.getProtectionDomain().getCodeSource().getLocation());
+            return false;
+        } catch (ClassNotFoundException e) {
+            task.getLogger().lifecycle("Skipping AJC (no inpath, no aspectpath, no aspectjrt on classpath)");
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     private AspectJCompileSpec createSpec(Task compile) {
         AspectJCompileSpec spec = new AspectJCompileSpec();
 
         if (compile instanceof AbstractCompile) {
             AbstractCompile abstractCompile = (AbstractCompile) compile;
             spec.setDestinationDir(abstractCompile.getDestinationDirectory().get().getAsFile());
-            spec.setCompileClasspath(new ArrayList<>(abstractCompile.getClasspath().filter(File::exists).getFiles()));
             spec.setTargetCompatibility(abstractCompile.getTargetCompatibility());
             spec.setSourceCompatibility(abstractCompile.getSourceCompatibility());
         }
         else if (compile instanceof KotlinJvmCompile) {
             KotlinJvmCompile kotlinJvmCompile = (KotlinJvmCompile) compile;
             spec.setDestinationDir(kotlinJvmCompile.getDestinationDirectory().get().getAsFile());
-            spec.setCompileClasspath(new ArrayList<>(kotlinJvmCompile.getLibraries().filter(File::exists).getFiles()));
             spec.setTargetCompatibility(kotlinJvmCompile.getKotlinOptions().getJvmTarget());
             spec.setSourceCompatibility(kotlinJvmCompile.getKotlinOptions().getJvmTarget());
         }
+        spec.setCompileClasspath(getCompileClasspath(compile).collect(Collectors.toList()));
 
         spec.setWorkingDir(projectLayout.getProjectDirectory().getAsFile());
         spec.setTempDir(compile.getTemporaryDir());
@@ -128,5 +173,29 @@ public class AjcAction implements Action<Task> {
         spec.setLauncher(launcher.getOrNull());
 
         return spec;
+    }
+
+    public static Stream<File> getCompileClasspath(Task task) {
+        FileCollection classpath = null;
+        if (task instanceof AbstractCompile) {
+            classpath = ((AbstractCompile) task).getClasspath();
+        }
+        else if (task instanceof KotlinJvmCompile) {
+            classpath = ((KotlinJvmCompile) task).getLibraries();
+        }
+
+        if (classpath != null) {
+            return classpath.getFiles()
+                    .stream()
+                    .filter(File::exists);
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+
+    @SneakyThrows
+    private static URL toURL(File file) {
+        return file.toURI().toURL();
     }
 }
---
+++ examples/aspectj/weaving/src/integrationTest/java/io/freefair/example/TestIT.java
@@ -0,0 +1,14 @@
+package io.freefair.example;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestIT {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
