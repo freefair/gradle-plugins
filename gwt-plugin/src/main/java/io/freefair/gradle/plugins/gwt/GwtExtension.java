102d45d1955d0d205e7e2301c044e3e3731ff306
Lars Grefer <github@larsgrefer.de>
2026-02-13 00:49:29 +0100

    Update to GWT 2.13.0

--- gwt-plugin/src/main/java/io/freefair/gradle/plugins/gwt/GwtExtension.java
+++ gwt-plugin/src/main/java/io/freefair/gradle/plugins/gwt/GwtExtension.java
@@ -13,6 +13,6 @@ public abstract class GwtExtension {
     public abstract ListProperty<String> getModules();
 
     public GwtExtension() {
-        getToolVersion().convention("2.12.2");
+        getToolVersion().convention("2.13.0");
     }
 }
