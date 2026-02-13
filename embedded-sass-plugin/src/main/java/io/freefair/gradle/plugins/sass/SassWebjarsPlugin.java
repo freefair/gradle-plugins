45a90122d332ce64abd7a0dac39fb4efd88e3f95
Lars Grefer <github@larsgrefer.de>
2026-02-13 01:01:26 +0100

    Improve Lazy Configuration

--- embedded-sass-plugin/src/main/java/io/freefair/gradle/plugins/sass/SassWebjarsPlugin.java
+++ embedded-sass-plugin/src/main/java/io/freefair/gradle/plugins/sass/SassWebjarsPlugin.java
@@ -1,6 +1,7 @@
 package io.freefair.gradle.plugins.sass;
 
 import org.gradle.api.Incubating;
+import org.gradle.api.NamedDomainObjectProvider;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
@@ -12,10 +13,13 @@ public class SassWebjarsPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         project.getPlugins().apply(SassBasePlugin.class);
-        Configuration webjars = project.getConfigurations().create("webjars");
+        NamedDomainObjectProvider<Configuration> webjars = project.getConfigurations().register("webjars");
 
         project.getPlugins().withType(JavaPlugin.class, javaPlugin ->
-                webjars.extendsFrom(project.getConfigurations().named(JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME).get())
+                webjars.configure(wj -> {
+                    Configuration compileClasspath = project.getConfigurations().named(JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME).get();
+                    wj.extendsFrom(compileClasspath);
+                })
         );
 
         project.getTasks().withType(SassCompile.class)
