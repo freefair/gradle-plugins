71025a114c3108f9cee16e14d12e31ef36b57258
jsolferreira <96002924+jsolferreira@users.noreply.github.com>
2026-08-23 13:14:32 +0100

    Bump default lombok version to 1.18.46 (#1817)
    * Bump default lombok version to 1.18.46
    
    * chore: trigger CI
    
    ---------
    
    Co-authored-by: Dennis Fricke <d.fricke@freefair.io>

--- lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/LombokExtension.java
+++ lombok-plugin/src/main/java/io/freefair/gradle/plugins/lombok/LombokExtension.java
@@ -11,7 +11,7 @@ import javax.inject.Inject;
  */
 public abstract class LombokExtension {
 
-    public static final String LOMBOK_VERSION = "1.18.44";
+    public static final String LOMBOK_VERSION = "1.18.46";
 
     /**
      * The version of Lombok which will be used.
